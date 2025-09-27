c437b8b680cd5d15ffe5f59d7b29df134977089d
rosnifarook <rosnifarook24@gmail.com>
2025-09-27 23:42:28 +0530

    Fix case-sensitive import for App.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/router.jsx";
 import "./index.css";
-import "./app.css"; // custom styles
+import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import "sweetalert2/dist/sweetalert2.js";
