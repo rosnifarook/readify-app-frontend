a15aba56c2285476a5a611ead31cd77ad309fddf
rosnifarook <rosnifarook24@gmail.com>
2025-09-28 00:11:21 +0530

    Fixed order redux issue

--- src/redux/features/orders/OrdersApi.js
+++ src/redux/features/orders/OrdersApi.js
@@ -1,5 +1,5 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import getBaseUrl from "../../../utils/baseURL";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 
 const ordersApi = createApi({
