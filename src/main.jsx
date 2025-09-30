24f9b44f0e8ff2efd371117a52db8d6970bbb184
rosnifarook <rosnifarook24@gmail.com>
2025-09-30 21:11:31 +0530

    Fiexed issues with Asgardeo Sign in

--- src/main.jsx
+++ src/main.jsx
@@ -10,26 +10,21 @@ import { store } from "./redux/store.js";
 import "sweetalert2/dist/sweetalert2.js";
 import { AuthProvider as AsgardeoAuthProvider } from "@asgardeo/auth-react";
 
-const scope = (import.meta.env.VITE_ASGARDEO_SCOPE || "openid profile email")
-  .split(" ")
-  .filter(Boolean);
-
-const asgardeoConfig = {
-  signInRedirectURL: import.meta.env.VITE_ASGARDEO_REDIRECT_URL,
-  signOutRedirectURL: import.meta.env.VITE_ASGARDEO_BASE_URL, // or use the callback if you prefer
-  clientID: import.meta.env.VITE_ASGARDEO_CLIENT_ID,
-  baseUrl: import.meta.env.VITE_ASGARDEO_SERVER_ORIGIN,
-  scope,
-  enablePKCE: true,
-};
-
 createRoot(document.getElementById("root")).render(
   // <Provider store={store}>
   //   <RouterProvider router={router} />
   // </Provider>
 
   //1st First step to use AsgardeoAuthProvider is to wrap the app with the provider component
-  <AsgardeoAuthProvider config={asgardeoConfig}>
+  <AsgardeoAuthProvider
+    config={{
+      signInRedirectURL: "http://localhost:5173/auth/callback",
+      signOutRedirectURL: "http://localhost:5173",
+      clientID: "8eqotwYJ8B9h9wCOOp9s5HfK0tQa",
+      baseUrl: "https://api.asgardeo.io/t/rosnifarook",
+      scope: ["openid", "profile"],
+    }}
+  >
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
