82daa16eb5d8ab752e8c2838cc2f688718811a43
rosnifarook <rosnifarook24@gmail.com>
2025-10-01 09:00:22 +0530

    Added .env.production file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -10,9 +10,11 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
+
+console.log("Firebase API key:", import.meta.env.VITE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
